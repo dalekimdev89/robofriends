eb372290838cbd89a56b18ba48db481faa2a2f1b
dkim <dkim@thedmsgrp.com>
2020-07-16 14:55:03 -0400

    redux middleware thunk

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,33 +6,27 @@ import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = state => {
 	return {
-		searchField: state.searchField
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
 	}
 }
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
 	}
 }
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: []
-			// searchfield: ''
-		}
-	}
-
 	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then(response => response.json())
-			.then(users => this.setState({ robots: users }));
+		this.props.onRequestRobots();
 	}
 
 	// onSearchChange = (event) => {
@@ -40,13 +34,12 @@ class App extends Component {
 	// }
 
 	render() {
-		const { robots } = this.state;
-		const { searchField, onSearchChange } = this.props;
-		const filteredRobots = this.state.robots.filter(robot => {
+		const { searchField, onSearchChange, robots, isPending, error } = this.props;
+		const filteredRobots = this.props.robots.filter(robot => {
 			return robot.name.toLowerCase().includes(searchField.toLowerCase())
 		})
 
-		return !robots.length ?
+		return isPending ?
 			<h1>Loading...</h1> :
 			(
 				<div className="tc">
